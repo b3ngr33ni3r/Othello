2a5464471ab0883d243205b50fc428b8838ddc76
B3n <mygithub@b3nhub.com>
2011-11-17 16:02:32 -0500

    updated via github web ui to fix comments and #define to be active.

--- framework.cpp
+++ framework.cpp
@@ -1,13 +1,13 @@
 #include "framework.h"
 #include "vectorController.h"
 
-//#define vectorController vectorController::get()
+#define vectorController vectorController::get()
 
 
 framework::framework()
 {
     //ctor
-    vectorController::get()->newNode();
+    vectorController->newNode();
     NUMBERCELLS=64;
 }
 
@@ -15,7 +15,7 @@ void framework::init_board()
 {
     for (int i=0;i<NUMBERCELLS;i++)
     {
-        /*
+        
         vectorController->newNode();
         vectorController->cells[i].boolean["visible"]=false;
 
@@ -24,6 +24,7 @@ void framework::init_board()
         vectorController->cells[i].color["chipcolorp1"]=sf::Color(255,255,255);
         vectorController->cells[i].color["chipcolorp2"]=sf::Color(0,0,0);
 
-        vectorController->cells[i].integer["belongsto"]=0;//1 for p1,2 for p2 */
+        vectorController->cells[i].integer["belongsto"]=0;//1 for p1,2 for p2 
+        
     }
 }
