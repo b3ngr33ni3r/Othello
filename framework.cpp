9e172f749cfa968da3af778bd28a39ae356b80fc
Ben Greenier <mygithub@b3nhub.com>
2011-11-17 15:00:09 -0500

    pushed from B3n's Desktop computer @ 15:00:09 on 11/17/11.

--- framework.cpp
+++ framework.cpp
@@ -1,4 +1,5 @@
 #include "framework.h"
+#include "vectorController.h"
 
 framework::framework()
 {
--- framework.h
+++ framework.h
@@ -1,5 +1,6 @@
 #ifndef FRAMEWORK_H
 #define FRAMEWORK_H
+#include "vectorController.h"
 /*
 *   Put things in here, to clean up the code.
 *
@@ -13,6 +14,7 @@ class framework
 
     protected:
     private:
+        vectorController vector_clone;
 };
 
 #endif // FRAMEWORK_H
